61f891aa2cb6308d7db6d223d516d6115be8a9e8
tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>
2024-10-15 07:35:41 +0300

    chore: initial
    Signed-off-by: tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>

---
+++ tailwind.config.mjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    backgroundSize: {
+      'size-2x': '200%',
+    },
+    extend: {
+      animation: {
+        rotate: 'rotate 10s linear infinite',
+        rainbow: 'rainbow 3s ease-in infinite',
+        'ping-2xs': 'ping 3s cubic-bezier(0, 0, 0.5, 0.8) infinite;',
+        'rotate-2xs': 'rainbow 10s cubic-bezier(0, 0, 0.5, 0.8) infinite;',
+      },
+      keyframes: {
+        rotate: {
+          '0%': { transform: 'rotate(0deg) scale(10)' },
+          '100%': { transform: 'rotate(-360deg) scale(10)' },
+        },
+        rainbow: {
+          '0%': { 'background-position': '0' },
+          '50%': { 'background-position': '100%' },
+          '100%': { 'background-position': '0' },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
