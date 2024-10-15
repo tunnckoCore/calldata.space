ccd87ea2b46b409bd4a3b46a0beb8754c626433e
tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>
2024-10-15 20:02:19 +0300

    chore: try another stars
    Signed-off-by: tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,10 +10,20 @@ export default {
         rotate: 'rotate 10s linear infinite',
         rainbow: 'rainbow 3s ease-in infinite',
         'ping-2xs': 'ping 3s cubic-bezier(0, 0, 0.5, 0.8) infinite;',
-        'rotate-2xs': 'rainbow 10s cubic-bezier(0, 0, 0.5, 0.8) infinite;',
+        'rotate-cubic-10s': 'rainbow 10s cubic-bezier(0, 0, 0.5, 0.8) infinite;',
+        'rotate-linear-slow': 'rotate 50s cubic-bezier(0, 0, 0.5, 0.8) infinite;',
         'spin-2xs': 'ping 750s linear infinite;',
+        movingbg: 'movingbg 650s linear infinite',
       },
       keyframes: {
+        movingbg: {
+          from: {
+            transform: 'translate3d(0px, 0px, 0px)',
+          },
+          to: {
+            transform: 'translate3d(1000px, 0px, 0px)',
+          },
+        },
         rotate: {
           '0%': { transform: 'rotate(0deg) scale(10)' },
           '100%': { transform: 'rotate(-360deg) scale(10)' },
@@ -21,7 +31,7 @@ export default {
         rainbow: {
           '0%': { 'background-position': '0' },
           '50%': { 'background-position': '100%' },
-          '100%': { 'background-position': '0' },
+          '100%': { 'background-position': '50%' },
         },
       },
     },
