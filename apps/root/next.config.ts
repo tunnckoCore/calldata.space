d21fd45d6b1cd3c6e6a6089a16d287be3556f15a
tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>
2024-12-27 19:30:39 +0200

    fix: rewrite "api." subdomain to mainnet proxy app
    Signed-off-by: tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>

--- apps/root/next.config.ts
+++ apps/root/next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   experimental: { reactCompiler: true, dynamicIO: true, ppr: true },
+
+  async rewrites() {
+    return [
+      {
+        has: [{ type: 'host', value: 'api.calldata.space' }],
+        source: '/:path*',
+        destination: 'https://mainnet.api.calldata.space/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
