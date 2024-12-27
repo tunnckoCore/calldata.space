b15b698320426d3c94f3155871a77f48248b8b88
tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>
2024-12-27 20:22:46 +0200

    chore: wtf.. tailwind doesnt get loaded
    Signed-off-by: tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>

--- packages/ethscriptions-core/dist/constants.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const BASE_API_URL = "https://api.ethscriptions.com/v2";
-export declare const CACHE_TTL = 3600;
--- packages/ethscriptions-server/dist/routes.d.ts
+++
@@ -1,4 +0,0 @@
-import { type Context } from "hono";
-import { createApp } from "./helpers.ts";
-export declare function getEnv(ctx: Context, key?: string): string;
-export declare function withRoutes(app: ReturnType<typeof createApp>, baseURL?: string): ReturnType<typeof createApp>;
